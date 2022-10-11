0a7861902cf5f82e4e3e06f642c6a0a34c1c3410
vega <vegawong1024@gmail.com>
2022-10-11 11:15:51 +0800

    feat: 开启相对行号

--- init.lua
+++ init.lua
@@ -2,8 +2,13 @@
 -- override leader key setting
 vim.g.mapleader = ','
 
+local opt = vim.opt
+
 -- autocmds
 local autocmd = vim.api.nvim_create_autocmd
+
+-- 开启相对行号
+opt.relativenumber = true
 
 -- 根据文件名识别jsonc格式
 autocmd({"BufEnter", "BufWinEnter"}, {
